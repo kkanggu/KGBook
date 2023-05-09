bc6ec38d86298d86b9b93d88bee79cb46dcdea5b
kkanggu <rica742244@gmail.com>
2023-05-09 21:18:22 +0900

    chore: initial setting

---
+++ src/main/java/kkanggu/KGBook/KgBookApplication.java
@@ -0,0 +1,13 @@
+package kkanggu.KGBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgBookApplication.class, args);
+	}
+
+}
