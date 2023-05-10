19b4e37f6d6a5f533a3a51416745352d8e0ce673
kkanggu <rica742244@gmail.com>
2023-05-10 20:09:37 +0900

    chore: update query to search books by ISBN instead of title

--- src/main/java/kkanggu/KGBook/sql/BookSql.java
+++ src/main/java/kkanggu/KGBook/sql/BookSql.java
@@ -5,5 +5,5 @@ public class BookSql {
 	public static String UPDATE_BOOK = "UPDATE BOOK SET title=?, author=?, publisher=?, publish_date=?, isbn=?, description=? WHERE id=?;";
 	public static String SELECT_BOOKS = "SELECT * FROM BOOK";
 	public static String SELECT_BOOKS_BY_ID = "SELECT * FROM BOOK WHERE id = ?";
-	public static String SELECT_BOOKS_BY_TITLE = "SELECT * FROM BOOK WHERE title = ?";
+	public static String SELECT_BOOKS_BY_ISBN = "SELECT * FROM BOOK WHERE isbn = ?";
 }
