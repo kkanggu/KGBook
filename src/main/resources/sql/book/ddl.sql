316319d859747511e7226872e43ae4560afd055e
kkanggu <rica742244@gmail.com>
2023-05-12 18:23:49 +0900

    feat: add USER table

--- src/main/resources/sql/book/ddl.sql
+++ src/main/resources/sql/book/ddl.sql
@@ -8,3 +8,13 @@ CREATE TABLE IF NOT EXISTS BOOK (
   original_image_url VARCHAR(100),
   s3_image_url VARCHAR(100)
 );
+
+CREATE TABLE IF NOT EXISTS USER (
+  id BIGINT NOT NULL PRIMARY KEY,
+  username VARCHAR(20) NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  gender CHAR(1),
+  age INT,
+  birth VARCHAR(8),
+  create_date TIMESTAMP NOT NULL
+);
