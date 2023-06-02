edbd3a60f9f246f671f8bcd2dc987d91b1dac04a
kkanggu <rica742244@gmail.com>
2023-06-02 22:47:47 +0900

    refactor: refactor JdbcBookRepositoryTest to unit test
    - Currently, `jdbcTemplate.update` is only mocking, not capturing.

--- src/test/java/kkanggu/KGBook/book/repository/JdbcBookRepositoryTest.java
+++ src/test/java/kkanggu/KGBook/book/repository/JdbcBookRepositoryTest.java
@@ -1,160 +1,149 @@
 package kkanggu.KGBook.book.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
-import javax.sql.DataSource;
+import java.util.stream.IntStream;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 import kkanggu.KGBook.book.entity.BookEntity;
 import kkanggu.KGBook.common.aws.ImageController;
-import kkanggu.KGBook.sql.BookSql;
 
-@SpringBootTest
-@ActiveProfiles("local")
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class JdbcBookRepositoryTest {
-	private final JdbcTemplate jdbcTemplate;
-	private final JdbcBookRepository jdbcBookRepository;
-	private final ImageController imageController;
+	@InjectMocks
+	private JdbcBookRepository repository;
 
-	@Autowired
-	public JdbcBookRepositoryTest(DataSource dataSource,
-								  ImageController imageController) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-		this.jdbcBookRepository = new JdbcBookRepository(dataSource, imageController);
-		this.imageController = imageController;
-	}
+	@Mock
+	private JdbcTemplate jdbcTemplate;
 
-	void insertBooksBeforeTest() {
-		for (int i = 0; i < 4; ++i) {
-			BookEntity book = BookEntity.builder()
-					.isbn(1357924680130L + i)
-					.title("book" + (i + 1))
-					.author("author" + (i + 1))
-					.publisher("publisher")
-					.originPrice(13579)
-					.publishDate(LocalDate.now()).createDate(LocalDate.now())
-					.description("description")
-					.originImageUrl("https://shopping-phinf.pstatic.net/main_3249079/32490791688.20221230074134.jpg")
-					.build();
-			jdbcBookRepository.saveBook(book);
-		}
-	}
+	@Mock
+	private ImageController imageController;
 
-	@BeforeEach
-	void clear() {
-		jdbcTemplate.update("DELETE FROM BOOK");
-	}
-
-	@Test
-	@DisplayName("서적 저장")
-	void saveBookTest() {
-		// given
-		BookEntity book = BookEntity.builder()
-				.isbn(1357924680134L)
-				.title("title")
+	private BookEntity getBookEntity(Long isbn) {
+		return BookEntity.builder()
+				.isbn(isbn)
+				.title("book")
 				.author("author")
 				.publisher("publisher")
 				.originPrice(13579)
 				.publishDate(LocalDate.now())
-				.createDate(LocalDate.now())
 				.description("description")
 				.originImageUrl("https://shopping-phinf.pstatic.net/main_3249079/32490791688.20221230074134.jpg")
 				.build();
-
-		// when
-		long isbn = jdbcBookRepository.saveBook(book);
-		BookEntity findBook = jdbcTemplate.queryForObject(BookSql.SELECT_BOOKS_BY_ISBN, rowMapper(), book.getIsbn());
-
-		// then
-		assertThat(isbn).isEqualTo(book.getIsbn());
-		assertThat(findBook).isNotNull();
-		assertThat(book.getIsbn()).isEqualTo(findBook.getIsbn());
-		assertThat(book.getTitle()).isEqualTo(findBook.getTitle());
-		assertThat(book.getAuthor()).isEqualTo(findBook.getAuthor());
-		assertThat(book.getPublisher()).isEqualTo(findBook.getPublisher());
-		assertThat(book.getOriginPrice()).isEqualTo(findBook.getOriginPrice());
-		assertThat(book.getPublishDate()).isEqualTo(findBook.getPublishDate());
-		assertThat(book.getDescription()).isEqualTo(findBook.getDescription());
-		assertThat(book.getOriginImageUrl()).isEqualTo(findBook.getOriginImageUrl());
-		assertThat(findBook.getS3ImageUrl()).isNotNull();
-
-		boolean isDeleted = imageController.deleteImage(findBook.getS3ImageUrl());
-		assertThat(isDeleted).isEqualTo(true);
 	}
 
 	@Test
-	@DisplayName("서적 전체 가져오기")
-	void findAllTest() {
-		// given
-		insertBooksBeforeTest();
+	@DisplayName("서적 저장 성공")
+	void saveBookOk() {
+		BookEntity book = getBookEntity(135L);
+		when(imageController.uploadImage(anyString())).thenReturn("");
+		when(jdbcTemplate.update(anyString(), ArgumentMatchers.<Object[]>any())).thenReturn(1);
 
-		// when
-		List<BookEntity> books = jdbcBookRepository.findAll();
+		Long isbn = repository.saveBook(book);
 
-		// then
-		assertThat(books.size()).isEqualTo(4);
-
-		for (BookEntity book : books) {
-			boolean isDeleted = imageController.deleteImage(book.getS3ImageUrl());
-			assertThat(isDeleted).isEqualTo(true);
-		}
+		assertAll(
+				() -> assertThat(isbn).isNotNull(),
+				() -> assertThat(isbn).isEqualTo(book.getIsbn())
+		);
 	}
 
 	@Test
-	@DisplayName("isbn을 이용하여 서적 가져오기")
-	void findByIsbnTest() {
-		// given
-		BookEntity book = BookEntity.builder()
-				.isbn(1357924680134L)
-				.title("title")
-				.author("author")
-				.publisher("publisher")
-				.originPrice(13579)
-				.publishDate(LocalDate.now())
-				.createDate(LocalDate.now())
-				.description("description")
-				.originImageUrl("https://shopping-phinf.pstatic.net/main_3249079/32490791688.20221230074134.jpg")
-				.build();
-		Long isbn = jdbcBookRepository.saveBook(book);
+	@DisplayName("서적 저장 실패, Image URL이 없을 경우")
+	void saveBookImageNotExist() {
+		BookEntity book = getBookEntity(135L);
+		when(imageController.uploadImage(anyString())).thenReturn(null);
 
-		// when
-		BookEntity findBook = jdbcBookRepository.findByIsbn(book.getIsbn());
+		Long isbn = repository.saveBook(book);
 
-		// then
-		assertThat(isbn).isEqualTo(book.getIsbn());
-		assertThat(findBook).isNotNull();
-		assertThat(findBook.getPublishDate()).isEqualTo(book.getPublishDate());
-
-		boolean isDeleted = imageController.deleteImage(findBook.getS3ImageUrl());
-		assertThat(isDeleted).isEqualTo(true);
+		assertAll(
+				() -> assertThat(isbn).isNull()
+		);
 	}
 
-	RowMapper<BookEntity> rowMapper() {
-		return (rs, rowNum) -> BookEntity.builder()
-				.isbn(rs.getLong("isbn"))
-				.title(rs.getString("title"))
-				.author(rs.getString("author"))
-				.publisher(rs.getString("publisher"))
-				.originPrice(rs.getInt("original_price"))
-				.discountPrice(rs.getInt("discount_price"))
-				.discountRate(rs.getInt("discount_rate"))
-				.discountType(rs.getString("discount_type"))
-				.publishDate(rs.getTimestamp("publish_date").toLocalDateTime().toLocalDate())
-				.createDate(LocalDate.now())
-				.description(rs.getString("description"))
-				.originImageUrl(rs.getString("original_image_url"))
-				.s3ImageUrl(rs.getString("s3_image_url"))
-				.build();
+	@Test
+	@DisplayName("서적 저장 실패, 모종의 이유로 저장 실패")
+	void saveBookImageFail() {
+		BookEntity book = getBookEntity(135L);
+		when(imageController.uploadImage(anyString())).thenReturn("");
+		when(jdbcTemplate.update(anyString(), ArgumentMatchers.<Object[]>any())).thenReturn(0);
+
+		Long isbn = repository.saveBook(book);
+
+		assertAll(
+				() -> assertThat(isbn).isNull()
+		);
+	}
+
+	@Test
+	@DisplayName("전체 서적 가져오기 성공")
+	void findAllOk() {
+		List<BookEntity> books = new ArrayList<>();
+		BookEntity book = getBookEntity(135L);
+		books.add(book);
+		when(jdbcTemplate.query(anyString(), any(RowMapper.class))).thenReturn(books);
+
+		List<BookEntity> findBooks = repository.findAll();
+
+		assertAll(
+				() -> assertThat(findBooks).isNotNull(),
+				() -> assertThat(findBooks.size()).isEqualTo(books.size()),
+				() -> IntStream.range(0, findBooks.size())
+						.forEach(i -> assertThat(findBooks.get(i)).isEqualTo(books.get(i)))
+		);
+	}
+
+	@Test
+	@DisplayName("전체 서적 가져오기 성공, 아무것도 없을 경우")
+	void findAllEmpty() {
+		when(jdbcTemplate.query(anyString(), any(RowMapper.class))).thenReturn(new ArrayList());
+
+		List<BookEntity> findBooks = repository.findAll();
+
+		assertAll(
+				() -> assertThat(findBooks).isNotNull(),
+				() -> assertThat(findBooks.isEmpty()).isTrue()
+		);
+	}
+
+	@Test
+	@DisplayName("Isbn을 이용하여 서적 가져오기 성공")
+	void findByIsbnOk() {
+		BookEntity book = getBookEntity(135L);
+		when(jdbcTemplate.queryForObject(anyString(), any(RowMapper.class), any())).thenReturn(book);
+
+		BookEntity findBook = repository.findByIsbn(book.getIsbn());
+
+		assertAll(
+				() -> assertThat(findBook).isNotNull(),
+				() -> assertThat(findBook).isEqualTo(book)
+		);
+	}
+
+	@Test
+	@DisplayName("Isbn을 이용하여 서적 가져오기 실패")
+	void findByIsbnFail() {
+		Long isbn = 13L;
+		when(jdbcTemplate.queryForObject(anyString(), any(RowMapper.class), any())).thenReturn(null);
+
+		BookEntity findBook = repository.findByIsbn(isbn);
+
+		assertAll(
+				() -> assertThat(findBook).isNull()
+		);
 	}
 }
